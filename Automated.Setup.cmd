53a104a73c836941c0f589a2eaac20d60fb4e0d7
Lee Foulger <lee.foulger.94@gmail.com>
2014-08-05 00:11:05 +0100

    Added the files

---
+++ Automated.Setup.cmd
@@ -0,0 +1,51 @@
+@Echo Off
+SET forgeVer=10.13.0.1198
+:DLForge
+IF NOT EXIST "%CD%\forge-1.7.10-%forgeVer%-src.zip" (
+	wget -c --no-check-certificate --no-cookies "http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-%forgeVer%/forge-1.7.10-%forgeVer%-src.zip" -O forge-1.7.10-%forgeVer%-src.zip
+) ELSE (
+	GOTO DLJDK
+)
+:DLJDK
+IF NOT EXIST "%CD%\jdk-.zip" (
+	wget -c --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u65-b19/jdk-7u65-windows-i586.exe" -O jdk-.zip
+) ELSE (
+	GOTO DLEclipse
+)
+:DLEclipse
+IF NOT EXIST "%CD%\eclipse.zip" (wget -c --no-check-certificate --no-cookies "http://download.eclipse.org/technology/epp/downloads/release/luna/R/eclipse-jee-luna-R-win32-x86_64.zip" -O eclipse.zip
+) ELSE (
+	GOTO XForge
+)
+:XForge
+IF NOT EXIST "%CD%\forge-1.7.10-src" (
+	7z x forge-1.7.10-%forgeVer%-src.zip -oforge-1.7.10-src
+) ELSE (
+	GOTO XEclipse
+)
+:XEclipse
+IF NOT EXIST "%CD%\eclipse" (
+	7z x eclipse.zip
+) ELSE (
+	GOTO XJDK
+)
+:XJDK
+IF NOT EXIST "%CD%\jdk" (
+	7z x jdk-*.zip & MOVE /Y %CD%\tools.zip jdk.zip & 7z x jdk.zip -ojdk & DEL /Q %CD%\jdk.zip & GOTO UnpackJDK
+) ELSE (
+	GOTO ForgeSetup
+)
+:UnpackJDK
+for /r %%x in (*.pack) do %CD%\jdk\bin\unpack200 -r "%%x" "%%~dx%%~px%%~nx.jar"
+SETX JAVA_HOME "%CD%\jdk"
+SETX PATH "%JAVA_HOME%\bin"
+:ForgeSetup
+IF EXIST "%CD%\forge-1.7.10-src" (
+	PUSHD %CD%\forge-1.7.10-src & GOTO setupWorkspace
+)
+:setupWorkspace
+IF NOT EXIST "%CD%\build" (
+	%CD%\gradlew setupDecompWorkspace & %CD%\gradlew setupDevWorkspace & %CD%\gradlew eclipse & POPD & %COMSPEC% /c %CD%\..\StartEclipse.vbs
+) ELSE (
+	POPD & %COMSPEC% /c %CD%\..\StartEclipse.vbs
+)
---
+++ StartEclipse.vbs
@@ -0,0 +1,3 @@
+Set oShell = CreateObject ("Wscript.Shell") 
+
+oShell.Run ".\eclipse\eclipse.exe -showlocation -data .\forge-1.7.10-src\eclipse", 1, false 
